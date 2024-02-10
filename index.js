6d5072e0143c9625d9c41f106a640a05ad60c7a6
Navaharish <navaharish2005@gmail.com>
2024-02-10 17:53:42 +0530

    Question and Answer joke

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const generatorBtn = document.getElementById('generator_btn')
 const jokeContent = document.getElementById('joke_content')
-const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single"
+const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart"
 
 const generator = () => {
     fetch(url)
         .then(data => data.json())
-        .then(item =>
-            jokeContent.textContent = item.joke)
+        .then(item => jokeContent.innerHTML = `QUESTION : "${item.setup}" <br> ANSWER : "${item.delivery}"`)
 }
 
 generator()
